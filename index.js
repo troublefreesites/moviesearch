5ccc2e319098c791a853c672013db13cd9470c87
troublefreesites <troublefreesites@gmail.com>
2021-04-18 22:46:01 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const https = require('https')
+app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const { error } = require('console');
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static('public'));
+var request = require("request");
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/result', (req, res) => {
+    console.log(req.body.title);
+    const apiKey = "f1dc652";
+    let title = req.body.title;
+    let reqUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${title}`
+    console.log(reqUrl);
+    request(reqUrl, (error, response, body) => {
+      
+
+      
+        var movieData = JSON.parse(body);
+        console.log(movieData);
+        if(movieData.Response == 'False'){
+          console.log("not a movie");
+          res.render('error');
+         
+        }
+        else {
+        res.render('result', {movieData});
+        }
+        
+        
+      
+    });
+    });
+
+  
+
+
+app.listen(3200, () => {
+  console.log('Example app listening on port 3200!')
+});
